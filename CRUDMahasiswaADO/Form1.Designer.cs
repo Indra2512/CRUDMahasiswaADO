b20e00ff1fa731a56e4113fef141a71465a2a3d3
Indra2512 <mononeon1225@gmail.com>
2026-04-23 20:50:39 +0700

    Menampilkan data ke Form

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -157,6 +157,7 @@
             this.dataGridView1.RowTemplate.Height = 28;
             this.dataGridView1.Size = new System.Drawing.Size(964, 220);
             this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
