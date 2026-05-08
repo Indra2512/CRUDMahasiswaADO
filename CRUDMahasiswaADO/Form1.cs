7f1d29fa1ec8d6d18e07b4aed981998af0dce5d3
Indra2512 <mononeon1225@gmail.com>
2026-05-08 11:36:04 +0700

    perbaikan

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -103,6 +103,7 @@ namespace CRUDMahasiswaADO
                         conn.Close();
                     }
                     MessageBox.Show("Data berhasil diupdate");
+                    LoadData();
                   
                 }
             }
